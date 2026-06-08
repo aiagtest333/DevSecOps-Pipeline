4801c63abead6438e1dbc61eddc9a90fad36ad5f
aiagtest333 <aiagtest@outlook.com>
2026-06-08 13:31:44 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 public class App {
+    static final String AWS_KEY = "AKIAIOSFODNN7EXAMPLEZ";
+    static final String AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPXRfiCYEXAMPLEKEY";
+
     public static void main(String[] args) {
         System.out.println("DevSecOps Pipeline Demo");
     }
