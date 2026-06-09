04eb3e10e8cc5a119290d0a3407495a247b57d9c
aiagtest333 <aiagtest@outlook.com>
2026-06-09 16:10:08 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,5 @@ package com.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("DevSecOps Pipeline Demo");
-        String s = "sk_test_51TgN0W1jz69YIAV7sYdHfvfuW8A1wF730WS9qxE3KY5Pxs6H4JFrIN6Uox5KBHAsAUl26ygfL5KACscV0gRNApNL00xoZhF9Fp";
     }
 }
