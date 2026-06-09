69fb2e9e3759fd64e92f5871f927ee4e2910b38d
aiagtest333 <aiagtest@outlook.com>
2026-06-09 14:33:26 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,6 @@
 package com.demo;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class App {
-    public static void getUser(Connection conn, String userId) throws Exception {
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE id = " + userId);
-    }
-
     public static void main(String[] args) {
         System.out.println("DevSecOps Pipeline Demo");
     }
